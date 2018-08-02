4537702adb619a034815767e0f38a8d26ac71161
ElDainosor <eldainosor@gmail.com>
2018-08-02 05:51:42 +0000

    fix bad derp

--- src/com/android/settings/display/FontPickerPreferenceController.java
+++ src/com/android/settings/display/FontPickerPreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
-import org.aospextended.extensions.AccentPicker;
+import com.bootleggers.dumpster.external.AccentPicker;
 
 import android.app.Fragment;
 import android.content.Context;
